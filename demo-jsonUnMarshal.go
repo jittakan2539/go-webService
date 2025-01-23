101444ba85dfde4ccef8334e0f4f709e5183f33c
Kan Jittapramoulboon <kan.j@themather.asia>
2025-01-23 16:32:34 +0700

    create Json UnMarshal

---
+++ demo-jsonUnMarshal.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type employee struct {
+	ID           int
+	EmployeeName string
+	Tel          string
+	Email        string
+}
+
+func main() {
+
+	myEmployee := employee{}
+	err := json.Unmarshal([]byte(`{"ID":1, "EmployeeName":"Peter", "Tel":"0900000000", "Email":"peter@mail.com"}`), &myEmployee)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(myEmployee)
+}
